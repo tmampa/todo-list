0aa317fb164584d8acbc6d3d5c587b212e5b91b7
tmampa <tmampa94@gmail.com>
2021-12-15 15:49:13 +0200

    Ad test  for the add function

--- src/add_remove.js
+++ src/add_remove.js
@@ -1,13 +1,10 @@
 function addTask(taskArray) {
   const inputList = document.getElementById('inputList');
-  if (inputList.value === '') {
-    return;
-  }
   const task = {
     description: inputList.value,
     completed: false,
     index: taskArray.length + 1,
-  };
+  }
   taskArray.push(task);
   localStorage.setItem('tasks', JSON.stringify(taskArray));
   inputList.value = '';
