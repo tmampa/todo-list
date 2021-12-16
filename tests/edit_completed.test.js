7707214c061159b67a557379658133675ab43927
Affaxed <delhinrharl@gmail.com>
2021-12-16 17:35:18 +0000

    Add clear  and update test functionality

--- tests/edit_completed.test.js
+++ tests/edit_completed.test.js
@@ -17,7 +17,6 @@ describe('editing a task', () => {
       completed: false,
       index: 2,
     },
-
   ];
 
   document.body.innerHTML = '<p class="description" id="para"></p>';
@@ -51,5 +50,49 @@ describe('updating the completed status of a task', () => {
       completed: false,
       index: 2,
     },
-
   ];
+  test('Update completed status to true', () => {
+    checkbox.checked = true;
+    status(checkbox, myTasks[1]);
+    expect(myTasks[1].completed).toBeTruthy();
+  });
+
+  test('Change completed status to false', () => {
+    checkbox.checked = false;
+    status(checkbox, myTasks[1]);
+    expect(myTasks[1].completed).toBeFalsy();
+  });
+});
+
+describe('clear completed tasks', () => {
+  let myTasks = [
+    {
+      description: 'Walk The Dog',
+      completed: true,
+      index: 1,
+    },
+
+    {
+      description: 'Do Laundry',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'Run Away',
+      completed: true,
+      index: 3,
+    },
+  ];
+
+  myTasks = myTasks.filter((task) => !task.completed);
+
+  test('Check array length after clearing completed tasks', () => {
+    clearChecked(myTasks);
+    expect(myTasks).toHaveLength(1);
+  });
+
+  test('Update index after clearing completed tasks', () => {
+    clearChecked(myTasks);
+    expect(myTasks[0].index).toBe(1);
+  });
+});
